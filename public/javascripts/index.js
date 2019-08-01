374c477b649ccd21374eb68f9fd723750aec1cd8
Elias Rosales <eliasrosales88@gmgmail.com>
2019-08-01 21:00:50 +0200

    Adding filters to index route

---
+++ public/javascripts/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function init() {
+    console.log('DOM loaded');
+    
+    // let results = document.getElementById('results').innerHTML;
+    // let paginationItems = document.querySelectorAll('.pagination .page-item')
+    
+    // if (parseInt(results) === 0) {
+    //     paginationItems.forEach(item => item.classList.add('disabled'));
+    //     console.log(paginationItems);
+    // }
+    
+
+
+
+}
+
+document.addEventListener("DOMContentLoaded", init);
--- routes/index.js
+++ routes/index.js
@@ -1,72 +1,81 @@
 var express = require('express');
-// var adverts = require('../routes/apiv1/adverts')
 var Advert = require('../models/Advert');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async (req, res, next) => {
-  
-  const adverts = await Advert.find();
 
-  try {
+router.get('/:page?', async (req, res, next) => {
+    // Pagination
+    let perPage = 5;
+    let page = req.params.page || 1;
+    const adverts = await Advert.find({});
 
-    const name = req.query.name;
-    const price = req.query.price;
-    const isSelled = req.query.isSelled;
-    const isSearched = req.query.isSearched;
-    const picture = req.query.picture;
-    const tags = req.query.tags;
-    const skip = parseInt(req.query.skip);
-    const limit = parseInt(req.query.limit); // eliminamos parseInt y forzamos error con limit 
-    const fields = req.query.fields;
-    const sort = req.query.sort;
+    try {
+        
+        // Query string 
+        const title = req.query.title;
+        const price = req.query.price;
+        const isSelled = req.query.isSelled;
+        const picture = req.query.picture;
+        const tags = req.query.tags;
+        const skip = parseInt(req.query.skip)  || (perPage * page) - perPage;
+        const limit = parseInt(req.query.limit) || perPage;
+        const fields = req.query.fields;
+        const sort = req.query.sort;
 
-    const filter = {};
-    
-    if (name) {
-        filter.name = name;
-    }
+        const filter = {};
 
-    // Check number type 
-    if (typeof price !== 'undefined') {
-        filter.price = price;
-    }
-
-    if (isSelled) {
-        filter.isSelled = isSelled;
-    }
-
-    if (isSearched) {
-        filter.isSearched = isSearched;
-    }
-    
-    if (tags) {
-
-        for (let i = 0; i < tags.length; i++) {
-            
-             filter.tags = tags[i];
+        if (title) {
+            filter.title = title;
         }
+
+        // Check number type 
+        if (typeof price !== 'undefined') {
+            filter.price = price;
+        }
+
+        if (isSelled) {
+            filter.isSelled = isSelled;
+        }
+
+    
+        if (tags) {
+
+            for (let i = 0; i < tags.length; i++) {
+
+                filter.tags = tags[i];
+            }
+
+        }
+    
+
+
+        const advertsFilter = await Advert.list({ filter: filter, skip, limit, fields, sort });
+        
+        console.log(advertsFilter);
+        
+        // Set locals to render
+        if (advertsFilter.length === 0) {
+            res.locals.advertsFilter = advertsFilter;
+            res.locals.pages = Math.ceil(advertsFilter.length / perPage);
+            res.locals.adverts = 'No results';
+            
+        } else {
+            res.locals.advertsFilter = advertsFilter;
+            res.locals.pages = Math.ceil(adverts.length / perPage);
+            res.locals.adverts = advertsFilter.length;
+        }
+        res.locals.current = page;
+        res.locals.filteredResults = Math.ceil(advertsFilter.length / perPage) ? Math.ceil(advertsFilter.length / perPage)  : 0 ;
+        res.render('index', { title: 'Nodepop' });
+
+    } catch (err) {
+        next(err);
     }
 
 
-    const advertsFilter = await Advert.list({filter: filter, skip, limit, fields, sort});// si colocamos llaves dentro de list podemos decir que el orden ya no es relevante
-    
-    console.log(advertsFilter.length);
-    
-    res.locals.adverts = advertsFilter;
-      
-    res.render('index', { title: 'Nodepop' });
-
-
-    // res.json({ success: true, results: adverts });
-} catch (err) {
-    next(err);
-}
-
-  
 
 });
-
 
 
 module.exports = router;
